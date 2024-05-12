105b0dc29de881d75abbf050c24ba6e26b26c435
Taras Shashurin <taras.shashurin.work@gmail.com>
2024-05-12 03:44:41 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Features
+
+This extension allows you to swap selections.
+
+## Keybindings
+
+Windows:
+<kbd>Ctrl</kbd> + <kbd>Alt</kbd> + <kbd>5</kbd>
+
+Mac:
+<kbd>⌘</kbd> + <kbd>5</kbd>
+
+Linux:
+<kbd>Ctrl</kbd> + <kbd>Alt</kbd> + <kbd>5</kbd>
+
+# Extension Settings
+
+This extension doesn't have any settings.
---
+++ src/extension.ts
@@ -0,0 +1,82 @@
+import * as vscode from "vscode";
+
+export function activate(context: vscode.ExtensionContext) {
+  const disposable = vscode.commands.registerCommand(
+    "vscode-selections-swap.swap",
+    () => {
+      const editor = vscode.window.activeTextEditor;
+
+      if (!editor) {
+        return;
+      }
+
+      if (editor.selections.length > 2) {
+        vscode.window.showErrorMessage("Too many selections (It should be 2)");
+        return;
+      }
+
+      if (editor.selections.length < 2) {
+        vscode.window.showErrorMessage(
+          "Not enough selections (It should be 2)"
+        );
+        return;
+      }
+
+      const initialSelections = editor.selections;
+
+      const isInitialSelectionsEmpty = initialSelections.every(
+        (selection) => selection.isEmpty
+      );
+
+      if (isInitialSelectionsEmpty) {
+        editor.selections = editor.selections.map((selection) => {
+          return new vscode.Selection(
+            selection.start.line,
+            0,
+            selection.end.line,
+            editor.document.lineAt(selection.end.line).text.length
+          );
+        });
+      }
+
+      const selectionsWithText = editor.selections.map((selection) => {
+        return {
+          selection,
+          text: editor.document.getText(selection),
+        };
+      });
+
+      if (isInitialSelectionsEmpty) {
+        editor.selections = initialSelections.map((selection, index) => {
+          return new vscode.Selection(
+            selection.start.line,
+            initialSelections[index === 0 ? 1 : 0].active.character,
+            selection.end.line,
+            initialSelections[index === 0 ? 1 : 0].active.character
+          );
+        });
+      }
+
+      const swappedSelections = [
+        {
+          selection: selectionsWithText[1].selection,
+          text: selectionsWithText[0].text,
+        },
+        {
+          selection: selectionsWithText[0].selection,
+          text: selectionsWithText[1].text,
+        },
+      ];
+
+      editor.edit((builder) => {
+        swappedSelections.forEach(({ selection, text }) => {
+          builder.replace(selection, text);
+        });
+      });
+    }
+  );
+
+  context.subscriptions.push(disposable);
+}
+
+export function deactivate() {}
